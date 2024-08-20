f5b248bc6968b47648e581ef1d6eaac2ab5cc1ca
MiguelBriseño <miguel.briseno.bustos@gmail.com>
2024-08-20 15:12:45 -0600

    Corregimos

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <script setup>
-import Navbar from './Components/Navbar/Navbar.vue';
-import Main from './Components/Main/Main.vue'
-import Contact from './Components/Contact/Contact.vue';
-import Hero from './Components/hero/Hero.vue'
-import Especialities from './Components/specialities/Especialities.vue';
-import Maps from './Components/maps/Maps.vue';
-import Carousel from './Components/carrusel/Carousel.vue';
-import Services from './Components/services/Services.vue';
-import Footer from './Components/footer/footer.vue';
+
 </script>
 
 <template>
-<Navbar></Navbar>
-<Hero></Hero>
-<Carousel />
-<Main></Main>
-<Especialities></Especialities>
-<Services />
-<Contact></Contact>
-<Maps></Maps>
-<Footer />
+    <div>
 
+    </div>
 </template>
 
 <style scoped>
--- src/Components/Layout.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="flex flex-col mt-20 justify-center items-center">
-
-    </div>
-</template>
-<script setup>
-
-</script>
